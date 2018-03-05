131b8a2c2b0f8d0a7e529f8831ce3881a8c3a61f
iqbal rubel <iqbalict39@outlook.com>
2018-03-05 22:04:56 +0600

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket=io();
+ socket.on('connect',function(){
+  console.log('connected to server');
+  // socket.emit('createEmail',{
+  //   to:'jen@example.com',
+  //   text:'Hey.This is Auto.'
+  // });
+socket.emit('createMessage',{
+  from:'Andrew',
+  rext:'Yup.that works for me'
+});
+
+});
+
+socket.on('disconnect',function(){
+  console.log('Disconnected from server');
+});
+
+// socket.on('newEmail',function(email){
+//   console.log('New email',email);
+// });
+
+socket.on('newMessage',function (message) {
+  console.log('newMessage',message);
+});
