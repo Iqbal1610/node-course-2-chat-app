65298a8214fcc01e3dddcdc3db83e88febdb81f9
iqbal rubel <iqbalict39@outlook.com>
2018-03-06 17:01:54 +0600

    Add  form for mrssages and show incoming messages in browsers

--- public/js/index.js
+++ public/js/index.js
@@ -22,4 +22,28 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function (message) {
   console.log('newMessage',message);
+
+  var li=jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+//for acknowledgement
+
+// socket.emit('createMessage',{
+//   from:'Frank',
+//   text:'hi'
+// },function(data){
+//   console.log('Got it',data);
+// });
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from:'User',
+    text:jQuery('[name=message]').val()
+  },function(){
+
+  });
 });
