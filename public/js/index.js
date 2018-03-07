ee7fb07bb5f0f37d4589f5631319aa50446adb5d
iqbal rubel <iqbalict39@outlook.com>
2018-03-07 22:43:50 +0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,24 @@
 var socket=io();
+
+
+function scrollToBottom(){
+  //selectors
+var messages=jQuery('#messages');
+var newMessage=messages.children('li:last-child');
+//heights
+var clientHeight=messages.prop('clientHeight');
+var scrollTop=messages.prop('scrollTop');
+var scrollHeight=messages.prop('scrollHeight');
+var newMessageHeight=newMessage.innerHeight();
+var lastMessageHeight=newMessage.prev().innerHeight();
+
+if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+messages.scrollTop(scrollHeight);
+}
+}
+
+
+
  socket.on('connect',function(){
   console.log('connected to server');
   // socket.emit('createEmail',{
@@ -30,7 +50,7 @@ var html=Mustache.render(template,{
 });
 jQuery('#messages').append(html);
 
-
+scrollToBottom();
 
   //console.log('newMessage',message);
  //var formatedTime=moment(message.createdAt).format('h:mm a');
@@ -50,7 +70,7 @@ var html=Mustache.render(template,{
 });
 jQuery('#messages').append(html);
 
-
+scrollToBottom();
   // var li= jQuery('<li></li>');
   // var a=jQuery('<a target="_blank">My current location</a>');
   //
